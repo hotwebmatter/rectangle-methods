5af466462bddc4eade4fb7884e1bf5071a94fd0d
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-18 15:19:49 -0500

    Use static System.Console

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using static System.Console;
 
 namespace rectangle_methods
 {
@@ -6,7 +7,7 @@ namespace rectangle_methods
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            WriteLine("Hello World!");
         }
     }
 }
