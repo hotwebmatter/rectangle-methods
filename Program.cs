261174fe83363f68fcede3ea60960382204d973f
Matt Obert on Windows <matt@hotwebmatter.com>
2019-12-18 15:18:44 -0500

    Initial commit of rectangle-methods project scaffold

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace rectangle_methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
